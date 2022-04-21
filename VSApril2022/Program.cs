b43c5dcae6882fef33c9c660deb0d4630320b3b1
Dona Sebastian <donapathikunnel@gmail.com>
2022-04-21 21:40:41 +1200

    first commit

---
+++ VSApril2022/Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace VSApril2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // open chrome browser
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            
+            // launch Turnup portal
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            // identify username textbox and enter valid username
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("Hari");
+
+            // identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            // click on login button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            // check if user is logged in successfully
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));  
+
+            if(helloHari.Text == "Hello hari!")
+            {
+                Console.WriteLine("Logged in successfully.Test passed");
+            }
+            else
+            {
+                Console.WriteLine("Test failed.");
+            }
+        }
+    }
+}
